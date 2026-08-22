1fbb54040f7664f7858b014bbc3006f978de7a51
Sujit SHEKHAR <shekharnalanda@gmail.com>
2026-08-22 15:19:21 +0530

    Fix learning portal heading assertion

--- tests/Feature/LearningResourceTest.php
+++ tests/Feature/LearningResourceTest.php
@@ -89,7 +89,7 @@ class LearningResourceTest extends TestCase
         ])->assertRedirect(route('student.dashboard'));
 
         $this->get(route('student.dashboard'))->assertOk()
-            ->assertSee('Study Materials &amp; Assignments', false)
+            ->assertSee('Study Materials & Assignments')
             ->assertSee('DCA Computer Fundamentals Notes')
             ->assertSee('https://example.com/dca-notes', false)
             ->assertDontSee('Private Tally Video')
