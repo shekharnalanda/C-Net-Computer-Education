1df3eb1e3530e2daad0cc186d323777281ff352f
Sujit SHEKHAR <shekharnalanda@gmail.com>
2026-08-22 12:03:01 +0530

    Allow course fee management

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -9,6 +9,6 @@ class Course extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['code','title','title_hi','duration','level','summary','eligibility','modules','careers','is_active','sort_order'];
-    protected $casts = ['modules'=>'array','careers'=>'array','is_active'=>'boolean'];
+    protected $fillable = ['code','title','title_hi','duration','fee_amount','fee_note','level','summary','eligibility','modules','careers','is_active','sort_order'];
+    protected $casts = ['fee_amount'=>'decimal:2','modules'=>'array','careers'=>'array','is_active'=>'boolean'];
 }
